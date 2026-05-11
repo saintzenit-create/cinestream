a36cd5f625caaa54d3dbf61efcb2e4620ea3037a
Rizal <queenserinnnn@gmail.com>
2026-05-11 20:41:48 +0700

    update

--- image-hosts.config.mjs
+++ image-hosts.config.mjs
@@ -1,49 +1,22 @@
-/**
- * Image Hosts Configuration
- */
-
 export const imageHosts = [
-
   {
     protocol: 'https',
-    hostname:
-      'images.unsplash.com',
+    hostname: 'images.unsplash.com',
   },
-
   {
     protocol: 'https',
-    hostname:
-      'images.pexels.com',
+    hostname: 'images.pexels.com',
   },
-
   {
     protocol: 'https',
-    hostname:
-      'images.pixabay.com',
+    hostname: 'images.pixabay.com',
   },
-
   {
     protocol: 'https',
-    hostname:
-      'img.rocket.new',
+    hostname: 'img.rocket.new',
   },
-
   {
     protocol: 'https',
-    hostname:
-      'blogger.googleusercontent.com',
+    hostname: 'xzloqvqsfjivsffzpjh.supabase.co',
   },
-
-  {
-    protocol: 'https',
-    hostname:
-      '*.supabase.co',
-  },
-
-  {
-    protocol: 'https',
-    hostname:
-      'clitore.com',
-  },
-
 ];

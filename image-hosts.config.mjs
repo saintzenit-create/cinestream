b9ae68e39845c8bc98bad2b8671f33f5f3c9c4f1
Rizal <queenserinnnn@gmail.com>
2026-05-11 20:05:19 +0700

    update config

--- image-hosts.config.mjs
+++ image-hosts.config.mjs
@@ -1,22 +1,49 @@
 /**
- * Image Hosts Configuration (add your image hosts here)
+ * Image Hosts Configuration
  */
 
 export const imageHosts = [
-    {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-    },
-    {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-    },
-    {
-        protocol: 'https',
-        hostname: 'images.pixabay.com',
-    },
-    {
-        protocol: 'https',
-        hostname: 'img.rocket.new',
-    },
+
+  {
+    protocol: 'https',
+    hostname:
+      'images.unsplash.com',
+  },
+
+  {
+    protocol: 'https',
+    hostname:
+      'images.pexels.com',
+  },
+
+  {
+    protocol: 'https',
+    hostname:
+      'images.pixabay.com',
+  },
+
+  {
+    protocol: 'https',
+    hostname:
+      'img.rocket.new',
+  },
+
+  {
+    protocol: 'https',
+    hostname:
+      'blogger.googleusercontent.com',
+  },
+
+  {
+    protocol: 'https',
+    hostname:
+      '*.supabase.co',
+  },
+
+  {
+    protocol: 'https',
+    hostname:
+      'clitore.com',
+  },
+
 ];

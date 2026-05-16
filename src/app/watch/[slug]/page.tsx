03298c0d5b996a4d6dcae319d70f6ea3511d7b36
Rizal <queenserinnnn@gmail.com>
2026-05-16 17:39:17 +0700

    forcedinsmic play

--- src/app/watch/[slug]/page.tsx
+++ src/app/watch/[slug]/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic =
+  "force-dynamic";
 import { notFound } from "next/navigation";
 import VideoPlayer from "@/components/VideoPlayer";
 import RealtimeViews from '@/components/RealtimeViews';

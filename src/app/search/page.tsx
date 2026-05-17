f13c6eaee4a7f6009287cd2f87a06ed3f9c02e19
Rizal <queenserinnnn@gmail.com>
2026-05-17 16:18:16 +0700

    search talent

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,6 @@
 import { searchVideos } from "@/lib/search";
 import VideoCard from "@/components/VideoCard";
+import Link from "next/link";
 
 export default async function SearchPage({
   searchParams,
@@ -16,6 +17,20 @@ export default async function SearchPage({
   const results = query
     ? await searchVideos(query)
     : [];
+
+  const talents = [
+  ...new Set(
+    results
+      .map((item: any) =>
+        item.talent
+      )
+      .filter(Boolean)
+  ),
+].filter((talent: any) =>
+  talent
+    .toLowerCase()
+    .includes(query.toLowerCase())
+);
 
   return (
     <main className="min-h-screen bg-black text-white">
@@ -45,6 +60,65 @@ export default async function SearchPage({
             Search
           </button>
         </form>
+
+        {talents.length > 0 && (
+
+  <div className="mb-10">
+
+    <h2 className="text-2xl font-bold mb-5">
+      Talent
+    </h2>
+
+    <div className="grid gap-4">
+
+      {talents.map((talent: any) => {
+
+        const talentVideo =
+          results.find(
+            (v: any) =>
+              v.talent === talent
+          );
+
+        return (
+
+          <Link
+            key={talent}
+            href={`/talent/${encodeURIComponent(talent)}`}
+            className="bg-zinc-900 hover:bg-zinc-800 transition rounded-2xl p-5 flex items-center gap-5"
+          >
+
+            <img
+              src={
+                talentVideo?.talent_image ||
+                "/placeholder.jpg"
+              }
+              alt={talent}
+              className="w-24 h-24 rounded-xl object-cover"
+            />
+
+            <div>
+
+              <h3 className="text-2xl font-black">
+                {talent}
+              </h3>
+
+              <p className="text-zinc-400 mt-1">
+                View Talent Profile
+              </p>
+
+            </div>
+
+          </Link>
+
+        );
+
+      })}
+
+    </div>
+
+  </div>
+
+)}
 
         {results.length > 0 ? (
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5">

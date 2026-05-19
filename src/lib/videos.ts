9d4907454c31b10df4e874bea523efed6af19acb
Rizal <queenserinnnn@gmail.com>
2026-05-19 22:49:34 +0700

    uptodate

--- src/lib/videos.ts
+++ src/lib/videos.ts
@@ -1,25 +1,23 @@
-import { supabase } from "@/lib/supabase";
+import { supabase }
+from "@/lib/supabase";
 
-export async function getAllVideos() {
-
-  const now =
-    new Date().toISOString();
+export async function
+getAllVideos() {
 
   const response =
     await supabase
       .from("videos")
       .select("*")
       .eq("status", "published")
-      .or(
-        `publish_at.is.null,publish_at.lte.${now}`
-      )
       .order("id", {
         ascending: false,
       });
 
   if (response.error) {
 
-    console.log(response.error);
+    console.log(
+      response.error
+    );
 
     return [];
 
